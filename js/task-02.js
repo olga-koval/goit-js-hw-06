56ae92cd48344b910725f8f53ff55407bdf106a7
Olga Koval <weddingkoval@gmail.com>
2022-02-03 19:20:22 +0200

    task 2 is added

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,6 @@
+
+// ASSIGNMENT #2
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +9,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// step#1 
+
+const list = document.querySelector("#ingredients");
+
+// step#2
+
+const title = document.createElement('h2');
+title.textContent = "Ingredients:";
+list.before(title);
+
+
+const elems = ingredients.map(ingredient => {
+  const ingredientItem = document.createElement("li"); // step#4
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item"); // step#3
+  return ingredientItem;
+});
+
+list.append(...elems);
+
+console.log(list);
+
